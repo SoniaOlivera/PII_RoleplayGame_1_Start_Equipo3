d7171d77357f86245beee4dc4377920e164b1e45
SoniaOlivera <61550104+SoniaOlivera@users.noreply.github.com>
2021-05-13 23:04:53 -0300

    agregados los test a la clase dwarf

--- src/Library/Dwarf.cs
+++ src/Library/Dwarf.cs
@@ -9,7 +9,9 @@ namespace Library
         private int hP;
         public int HP;
         private Axe weapon;
+        public Axe Weapon {get; set;}
         private Chestplate armor;
+        public Chestplate Armor {get; set;}
         private int baseAttackPower;
         private int baseDefensePower;
         private int baseHealingPower;
@@ -17,8 +19,8 @@ namespace Library
         {
             this.Name = name;
             this.HP = hP;
-            this.weapon = weapon;
-            this.armor = armor;
+            this.Weapon = weapon;
+            this.Armor = armor;
             this.baseAttackPower = baseAttackPower;
             this.baseDefensePower = baseDefensePower;
             this.baseHealingPower = baseHealingPower;
@@ -33,33 +35,33 @@ namespace Library
         }   
         public void RemoveAxe()
         {
-            this.weapon = null;
+            this.Weapon = null;
         }  
         public void ChangeAxe(Axe newaxe)
         {
             this.RemoveAxe();
-            this.weapon = newaxe;
+            this.Weapon = newaxe;
         }
         public void RemoveChestplate()
         {
-            this.armor = null;
+            this.Armor = null;
         }
         public void ChangeChestplate(Chestplate newchestplate)
         {
             this.RemoveChestplate();
-            this.armor = newchestplate;
+            this.Armor = newchestplate;
         }
         public void AttackDwarf(Dwarf target)
         {
-            target.SetHP(target.GetHP() + target.GetDefense() - this.baseAttackPower - this.weapon.GetDamage());
+            target.SetHP(target.GetHP() + target.GetDefense() - this.baseAttackPower -this.Armor.GetDamage() - this.Weapon.GetDamage());
         }
         public void AttackElf(Elf target)
         {
-            target.SetHP(target.GetHP() + target.GetDefense() - this.baseAttackPower - this.weapon.GetDamage());
+            target.SetHP(target.GetHP() + target.GetDefense() - this.baseAttackPower -this.Armor.GetDamage() - this.Weapon.GetDamage());
         }
         public void AttackWizard(Wizard target)
         {
-            target.SetHP(target.GetHP() + target.GetDefense() - this.baseAttackPower -this.armor.GetDamage() - this.weapon.GetDamage());
+            target.SetHP(target.GetHP() + target.GetDefense() - this.baseAttackPower -this.Armor.GetDamage() - this.Weapon.GetDamage());
         }
         public void HealDwarf(Dwarf target)
         {
@@ -75,7 +77,7 @@ namespace Library
         }
         public int GetDefense()
         {
-            return (this.baseDefensePower + this.weapon.GetArmor() + this.armor.GetArmor());
+            return (this.baseDefensePower + this.Weapon.GetArmor() + this.Armor.GetArmor());
         }
     }
 }
